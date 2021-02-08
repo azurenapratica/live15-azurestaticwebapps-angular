ac72474e95fa3524c013a39ddd77fb51f85c91bf
Milton Câmara <miltoncamaragomes@gmail.com>
2021-02-08 19:21:48 -0300

    Added API Function

--- api/Chamados.cs
+++ api/Chamados.cs
@@ -1,35 +1,43 @@
-using System;
 using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using ServerlessSuporte.Business;
 
-namespace Company.Function
+namespace ServerlessSuporte
 {
-    public static class Chamados
+    public class Chamados
     {
+        private readonly ChamadoServices _chamadoSvc;
+
+        public Chamados(ChamadoServices chamadoSvc)
+        {
+            _chamadoSvc = chamadoSvc;
+        }
+        
         [FunctionName("Chamados")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Acessada a Function Chamados");
+            log.LogInformation($"Operação: {req.Method}");
 
-            string name = req.Query["name"];
+            string strDadosOriginais =
+                new StreamReader(req.Body).ReadToEndAsync().Result;
+            log.LogInformation($"Dados originais: {strDadosOriginais}");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            switch (req.Method)
+            {
+                case "GET":
+                    return _chamadoSvc.Get();                
+                case "POST":
+                    return _chamadoSvc.Insert(strDadosOriginais);
+            }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new NotFoundResult();
         }
     }
 }
